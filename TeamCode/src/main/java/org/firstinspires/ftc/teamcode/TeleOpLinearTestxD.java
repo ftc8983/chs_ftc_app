ade74cb3203a6420e52181ffc581b2c8e05f4947
Thomas Lagace <thomas5051@live.com>
2016-10-14 12:31:59 -0400

    Added a nonworking linear OpMode to the brobot.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpLinearTestxD.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpLinearTestxD.java
@@ -33,33 +33,31 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.LegacyModule;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ServoController;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
- * This file contains an example of an iterative (Non-Linear) "OpMode".
- * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
- * The names of OpModes appear on the menu of the FTC Driver Station.
- * When an selection is made from the menu, the corresponding OpMode
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
  * class is instantiated on the Robot Controller and executed.
  *
  * This particular OpMode just executes a basic Tank Drive Teleop for a PushBot
- * It includes all the skeletal structure that all iterative OpModes contain.
+ * It includes all the skeletal structure that all linear OpModes contain.
  *
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Template: Iterative OpMode", group="Iterative Opmode")  // @Autonomous(...) is the other common choice
-//@Disabled
-public class OpMode_Iterative extends OpMode
-{
+@TeleOp(name="Test Linear Op", group="Linear Opmode")  // @Autonomous(...) is the other common choice
+
+public class TeleOpLinearTestxD extends LinearOpMode {
+
     /* Declare OpMode members. */
     private ElapsedTime runtime = new ElapsedTime();
 
@@ -69,11 +67,10 @@ public class OpMode_Iterative extends OpMode
     private ServoController servoCtrl = null;
     private LegacyModule legacyMod = null;
 
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
     @Override
-    public void init() {
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
 
         /* eg: Initialize the hardware variables. Note that the strings used here as parameters
          * to 'get' must correspond to the names assigned during the robot configuration
@@ -86,45 +83,29 @@ public class OpMode_Iterative extends OpMode
         legacyMod = hardwareMap.legacyModule.get("legacy module");
 
         // eg: Set the drive motor directions:
-        // Reverse the motor that runs backwards when connected directly to the battery
+        // "Reverse" the motor that runs backwards when connected directly to the battery
         // leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
-        //  rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
-        telemetry.addData("Status", "Initialized");
-    }
+        // rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
 
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-     */
-    @Override
-    public void init_loop() {
-    }
-
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
-    @Override
-    public void start() {
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
         runtime.reset();
-        //See if we can have the robot spit out some Liberty Prime quotes
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // eg: Run wheels in tank mode (note: The joystick goes negative when pushed forwards)
+            leftWheel.setPower(-gamepad1.left_stick_y);
+            rightWheel.setPower(-gamepad1.right_stick_y);
+
+            // eg: Run wheels in tank mode (note: The joystick goes negative when pushed forwards)
+            leftWheel.setPower(-gamepad1.left_stick_y);
+            rightWheel.setPower(-gamepad1.right_stick_y);
+            telemetry.addData("Right stick y value: " + gamepad1.right_stick_y, "\n" );
+            telemetry.addData("Right wheel power: " + rightWheel, "\n" );
+            telemetry.addData("Status", "Run Time: " + runtime.toString(), "\n");
+            telemetry.update();
+            idle(); // Always call idle() at the bottom of your while(opModeIsActive()) loop
+        }
     }
-
-    /*
-     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
-     */
-    @Override
-    public void loop() {
-        telemetry.addData("Status", "Running: " + runtime.toString());
-
-        // eg: Run wheels in tank mode (note: The joystick goes negative when pushed forwards)
-        leftWheel.setPower(-gamepad1.left_stick_y);
-        rightWheel.setPower(-gamepad1.right_stick_y);
-    }
-
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
-    @Override
-    public void stop() {
-    }
-
 }
